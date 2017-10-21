8b65d3d0196ab6396840f57937d42adb8c53cecd
Thomas <Thomas>
2017-10-21 20:44:55 +0200

    continued implementation of LanguageDependentTextInProductTableSetter

--- src/ui/LanguageDependentTextInProductTableSetter.js
+++ src/ui/LanguageDependentTextInProductTableSetter.js
@@ -10,14 +10,36 @@ assertNamespace('shop.ui');
  */
 shop.ui.LanguageDependentTextInProductTableSetter = function LanguageDependentTextInProductTableSetter(optionalUiComponentProvider, optionalBus) {
    
+   var addToShoppingCartButtonText;
+   var selectors = [];
+   
    var defaultUiComponentProvider = function defaultUiComponentProvider(selector) {
       return $(selector);
    };
    
    var uiComponentProvider = (optionalUiComponentProvider === undefined) ? defaultUiComponentProvider : optionalUiComponentProvider;
+   var bus = (optionalBus === undefined) ? shop.Context.bus : optionalBus;
+   
+   var updateButtons = function updateButtons() {
+      if (addToShoppingCartButtonText !== undefined) {
+         selectors.forEach(function(selector) {
+            uiComponentProvider(selector + ' button').text(addToShoppingCartButtonText);
+         });
+      }
+   };
+   
+   var onAddToShoppingCartButtonText = function onAddToShoppingCartButtonText(text) {
+      addToShoppingCartButtonText = (text !== undefined) ? text : '';
+      updateButtons();
+   };
    
    this.onTabContentChangedCallback = function onTabContentChangedCallback(selector) {
-      var components = uiComponentProvider(selector + ' button');
+      if (selectors.indexOf(selector) === -1) {
+         selectors[selectors.length] = selector;
+      }
+      updateButtons();
    };
+   
+   bus.subscribeToPublication(shop.topics.LANGUAGE_DEPENDENT_TEXT_PREFIX + 'addToShoppingCartButton', onAddToShoppingCartButtonText);
 };
 
--- test/ui/LanguageDependentTextInProductTableSetterTest.js
+++ test/ui/LanguageDependentTextInProductTableSetterTest.js
@@ -11,16 +11,22 @@ function valueIsAnObject(val) {
 }
 
 var DEFAULT_TAB_SELECTOR = 'defaultTabSelector';
+var DEFAULT_TAB2_SELECTOR = 'defaultTab2Selector';
+
 var instance;
 var publications;
 var capturedSubscriptionCallbacks;
-var capturedTabContentChangedCallbacks;
 var capturedSelectors;
+var capturedButtonTexts;
 var uiComponents;
 
 var mockedUiComponentProvider = function mockedUiComponentProvider(selector) {
    capturedSelectors[capturedSelectors.length] = selector;
-   return uiComponents;
+   return {
+      text: function text(content) {
+         capturedButtonTexts[capturedButtonTexts.length] = {selector: selector, text: content};
+      }
+   };
 };
 
 var mockedBus = {
@@ -34,11 +40,13 @@ var mockedBus = {
       if (callback !== undefined) {
          callback(data);
       }
+      publications[topic] = data;
    }
 };
 
-var MockedTab = function MockedTab() {
-   var selector = DEFAULT_TAB_SELECTOR;
+var MockedTab = function MockedTab(optionalSelector) {
+   var selector = (optionalSelector === undefined) ? DEFAULT_TAB_SELECTOR : optionalSelector;
+   var capturedTabContentChangedCallbacks = [];
    
    this.onTabContentChanged = function onTabContentChanged(callback) {
       capturedTabContentChangedCallbacks[capturedTabContentChangedCallbacks.length] = callback;
@@ -49,27 +57,59 @@ var MockedTab = function MockedTab() {
    };
 };
 
-var mockedTab = new MockedTab();
+var mockedTab;
+var mockedTab2;
 
 var givenDefaultLanguageDependentTextInProductTableSetter = function givenDefaultLanguageDependentTextInProductTableSetter() {
    instance = new shop.ui.LanguageDependentTextInProductTableSetter(mockedUiComponentProvider, mockedBus);
    mockedTab.onTabContentChanged(instance.onTabContentChangedCallback);
 };
 
-var givenTheDivContainsTheButtons = function givenTheDivContainsTheButtons(buttons) {
-   uiComponents = buttons;
+var givenAddToShoppingCartButtonTextIs = function givenAddToShoppingCartButtonTextIs(text) {
+   mockedBus.publish(shop.topics.LANGUAGE_DEPENDENT_TEXT_PREFIX + 'addToShoppingCartButton', text);
+};
+
+var givenTabContentChanges = function givenTabContentChanges() {
+   mockedTab.simulateChangeOfTabContent();
+};
+
+var givenContentOfBothTabsChanges = function givenContentOfBothTabsChanges() {
+   givenTabContentChanges();
+   mockedTab2.simulateChangeOfTabContent();
+};
+
+var givenAnotherTabGetsObservedByTheSetter = function givenAnotherTabGetsObservedByTheSetter() {
+   mockedTab2.onTabContentChanged(instance.onTabContentChangedCallback);
+};
+
+var whenAddToShoppingCartButtonTextIs = function whenAddToShoppingCartButtonTextIs(text) {
+   givenAddToShoppingCartButtonTextIs(text);
 };
 
 var whenTabContentChanges = function whenTabContentChanges() {
-   mockedTab.simulateChangeOfTabContent();
+   givenTabContentChanges();
+};
+
+var lastCapturedButtonTextIs = function lastCapturedButtonTextIs(expectedText, selector) {
+   var found = false;
+   
+   for (var index = 0; index < capturedButtonTexts.length; index++) {
+      if (selector === undefined || capturedButtonTexts[index].selector === (selector + ' button')) {
+         found = capturedButtonTexts[index].text === expectedText;
+      }
+   }
+   
+   return found;
 };
 
 var setup = function setup() {
    publications = {};
    capturedSubscriptionCallbacks = {};
-   capturedTabContentChangedCallbacks = [];
    capturedSelectors = [];
+   capturedButtonTexts = [];
    uiComponents = undefined;
+   mockedTab = new MockedTab();
+   mockedTab2 = new MockedTab(DEFAULT_TAB2_SELECTOR);
 };
 
 describe('LanguageDependentTextInProductTableSetter', function() {
@@ -89,12 +129,45 @@ describe('LanguageDependentTextInProductTableSetter', function() {
       expect(capturedSelectors.length).to.be.eql(1);
       expect(capturedSelectors[0]).to.be.eql(DEFAULT_TAB_SELECTOR + ' button');
    });
-   /*
-   it('when the tab content changes the buttons get updated', function() {
+   
+   it('when the tab content changes the buttons get updated A', function() {
       
-      givenTheDivContainsTheButtons([{id: 'button1'}, {id: 'button2'}]);
+      givenAddToShoppingCartButtonTextIs('in den Warenkorb');
       givenDefaultLanguageDependentTextInProductTableSetter();
       whenTabContentChanges();
-      expect(valueIsAnObject(instance)).to.be.eql(true);
-   });*/
+      expect(lastCapturedButtonTextIs('in den Warenkorb')).to.be.eql(true);
+   });
+   
+   it('when the tab content changes the buttons get updated B', function() {
+      
+      givenAddToShoppingCartButtonTextIs('a button text');
+      givenDefaultLanguageDependentTextInProductTableSetter();
+      whenTabContentChanges();
+      expect(lastCapturedButtonTextIs('a button text')).to.be.eql(true);
+   });
+   
+   it('when the tab content changes and the button text is undefined, then the buttons get updated with an empty text', function() {
+      
+      givenDefaultLanguageDependentTextInProductTableSetter();
+      whenTabContentChanges();
+      expect(lastCapturedButtonTextIs('')).to.be.eql(true);
+   });
+      
+   it('when the language dependent text changes the buttons get updated', function() {
+      
+      givenDefaultLanguageDependentTextInProductTableSetter();
+      givenTabContentChanges();
+      whenAddToShoppingCartButtonTextIs('new button text');
+      expect(lastCapturedButtonTextIs('new button text')).to.be.eql(true);
+   });
+      
+   it('when the language dependent text changes the buttons of previously changes tabs get updated', function() {
+      
+      givenDefaultLanguageDependentTextInProductTableSetter();
+      givenAnotherTabGetsObservedByTheSetter();
+      givenContentOfBothTabsChanges();
+      whenAddToShoppingCartButtonTextIs('some text');
+      expect(lastCapturedButtonTextIs('some text', DEFAULT_TAB_SELECTOR)).to.be.eql(true);
+      expect(lastCapturedButtonTextIs('some text', DEFAULT_TAB2_SELECTOR)).to.be.eql(true);
+   });
 });  
