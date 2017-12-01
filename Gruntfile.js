7fd9934844d3e6956dce60cbe24c8b77a9eac450
Thomas <Thomas>
2017-12-01 17:22:07 +0100

    extended Gruntfile to concatenate the javascript files and use it in the shop

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,10 @@ var fileSystem = require('fs');
 global.PROJECT_ROOT_PATH = path.resolve('.');
 global.PROJECT_SOURCE_ROOT_PATH = global.PROJECT_ROOT_PATH + '/src';
 global.PROJECT_TEST_ROOT_PATH   = global.PROJECT_ROOT_PATH + '/test';
-var CONCAT_FOLDER = global.PROJECT_ROOT_PATH + '/concat/';
-var CONCATENATED_FILE = CONCAT_FOLDER + 'webshop_spa.js';
+
+var JAVASCRIPT_TARGET_FOLDER = global.PROJECT_ROOT_PATH + '/webroot/javascripts';
+var CONCAT_FOLDER = JAVASCRIPT_TARGET_FOLDER;
+var CONCATENATED_FILE = CONCAT_FOLDER + '/webshop_spa.js';
 
 // Grunt is a JavaScript task runner, similar to Ant. 
 // See http://gruntjs.com/ for details
@@ -47,56 +49,53 @@ module.exports = function(grunt) {
       }
       },
       
-      clean: ['webroot/javascripts', CONCAT_FOLDER],
+      clean: [JAVASCRIPT_TARGET_FOLDER],
 
       copy: {
-         main: {
-            expand: true,
-            flatten: false,
-            cwd: 'src',
-            src: '**/*.js',
-            dest: 'webroot/javascripts'
-         }
+         // main: {
+         //    expand: true,
+         //    flatten: false,
+         //    cwd: 'src',
+         //    src: '**/*.js',
+         //    dest: 'webroot/javascripts'
+         // }
       },
       
       concat: {
-         options: {
-            process: function(src, filepath) {
-               var result = '';
-               var nameSpaceUtils = 'NamespaceUtils.js';
-               var index = filepath.indexOf(nameSpaceUtils);
-               
-               if (filepath.length < nameSpaceUtils.length || index !== (filepath.length - nameSpaceUtils.length)) {
-                  result = src;
-               }
-               return result;
-            }
-         },
          javascripts: {
             src: ['src/**/*.js'],
             dest: CONCATENATED_FILE,
-         },
+            filter: function(filepath) {
+               var nameSpaceUtils = 'NamespaceUtils.js';
+               return filepath.length < nameSpaceUtils.length ||
+                        filepath.indexOf(nameSpaceUtils) !== (filepath.length - nameSpaceUtils.length);
+            }
+         }
       }
    });
 
    grunt.loadNpmTasks('grunt-contrib-jshint');
    grunt.loadNpmTasks('grunt-mocha-test');
-   grunt.loadNpmTasks('grunt-contrib-copy');
+   //grunt.loadNpmTasks('grunt-contrib-copy');
    grunt.loadNpmTasks('grunt-contrib-clean');
    grunt.loadNpmTasks('grunt-contrib-concat');
    
    grunt.task.registerTask('correctConcatenatedFile', 'moves the prototype assignements to the and of the file and brings them into the right order', function() {
       var CRLF = '\r\n';
       var prototypeRegexp = /.*prototype =.*/g;
-      //require('./NamespaceUtils.js');
       var requireRegexp = /require\([^\(]*\);\s*\r?\n/g;
+      var linterSettingsRegexp = /\/\*\s*global[^\n]*\r?\n/g;
       var concatenatedContent = fileSystem.readFileSync(CONCATENATED_FILE, 'utf8');
       var namespaceUtilsContent = fileSystem.readFileSync(global.PROJECT_SOURCE_ROOT_PATH + '/NamespaceUtils.js', 'utf8');
       var prototypeAssignments = concatenatedContent.match(prototypeRegexp);
+      
       if (prototypeAssignments === null) {
          console.log('no prototype assignments found');
       } else {
-         var newContent = (namespaceUtilsContent + concatenatedContent).replace(prototypeRegexp, '').replace(requireRegexp, '');
+         var newContent = (namespaceUtilsContent + concatenatedContent)
+                              .replace(prototypeRegexp, '')
+                              .replace(requireRegexp, '')
+                              .replace(linterSettingsRegexp, '');
          fileSystem.writeFileSync(CONCATENATED_FILE, newContent, 'utf8');
          fileSystem.appendFileSync(CONCATENATED_FILE, CRLF + prototypeAssignments.join(CRLF), 'utf8');
          console.log(CRLF + '\tmoved ' + prototypeAssignments.length + ' prototype assignements to the end of ' + CONCATENATED_FILE);
@@ -106,6 +105,5 @@ module.exports = function(grunt) {
    grunt.registerTask('lint', ['jshint']);
    grunt.registerTask('format', ['jsbeautifier']);
    grunt.registerTask('test', ['mochaTest:libRaw']);
-   /* damit concat automatisch läuft gehört 'concat' zu den default hinzugefügt. */
-   grunt.registerTask('default', ['clean', 'lint', 'test', 'copy', 'concat', 'correctConcatenatedFile']);
+   grunt.registerTask('default', ['clean', 'lint', 'test', 'concat', 'correctConcatenatedFile']);
  };
