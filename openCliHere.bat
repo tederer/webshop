c69455cdf08e9a957b6009e05a35505281751f6f
EDERER Thomas <Thomas.EDERER@frequentis.com>
2017-12-07 16:44:38 +0100

    extended start scripts

--- openCliHere.bat
+++ openCliHere.bat
@@ -1,5 +1,27 @@
 @echo off
-set PATH=%PATH%;D:\software\Git\PortableGit\cmd
 set SCRIPTPATH=%~dp0
+set GIT_HOME=D:\software\Git\PortableGit\cmd
+set NODE_HOME=D:\software\Node.js\node-v8.9.2-win-x64
+set GRUNT_HOME=%SCRIPTPATH%\node_modules\grunt\node_modules\.bin
+set PATH=%PATH%;%GIT_HOME%;%NODE_HOME%;%GRUNT_HOME%
+
+if not exist %GIT_HOME% (
+   echo Home folder "%GIT_HOME%" of GIT does not exist!
+   pause
+   exit 1
+)
+
+if not exist %NODE_HOME% (
+   echo Home folder "%NODE_HOME%" of node.js does not exist!
+   pause
+   exit 1
+)
+
+if not exist %GRUNT_HOME% (
+   echo Home folder "%GRUNT_HOME%" of grunt does not exist!
+   pause
+   exit 1
+)
+
 echo %SCRIPTPATH%
 start cmd /k cd /d %SCRIPTPATH%
--- startWebserver.bat
+++ startWebserver.bat
@@ -1,13 +1,21 @@
 @echo off
 set BASEDIR=%~dp0
+set NODE_HOME=D:\software\Node.js\node-v8.9.2-win-x64
+set PATH=%PATH%;%NODE_HOME%
 if %BASEDIR:~-1,1% NEQ \ set BASEDIR=%BASEDIR%\
 
 title webserver
+
+if not exist %NODE_HOME% (
+   echo Home folder "%NODE_HOME%" of node.js does not exist!
+   pause
+   exit 1
+)
 
 set WEBROOTFOLDER=%BASEDIR%webroot
 
 if not exist %WEBROOTFOLDER% md %WEBROOTFOLDER%
 
-node D:\JavaScriptDevelopment\webserver\src\start.js %WEBROOTFOLDER%
+node %BASEDIR%\..\webserver\src\start.js %WEBROOTFOLDER%
 
 if errorlevel 1 pause
